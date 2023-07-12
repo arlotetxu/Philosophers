b6d23bbbc4d892f80d4d9430226db157f4a27311
ArLoTeTxU <arlotetxu@pm.me>
2023-07-12 22:04:57 +0200

    Understanding threads

---
+++ inc/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jflorido <jflorido@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 14:23:18 by jflorido          #+#    #+#             */
+/*   Updated: 2023/07/11 14:23:18 by jflorido         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_PHILO_H
+#define PHILOSOPHERS_PHILO_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#endif //PHILOSOPHERS_PHILO_H
---
+++ learning/first_thread.c
@@ -0,0 +1,40 @@
+//
+// Created by Jose Manuel Florido Pereña on 11/7/23.
+//
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void	*my_turn()
+{
+	while (1)
+	{
+		sleep(1);
+		printf("My turn.\n");
+	}
+	return (NULL);
+}
+
+void	*your_turn()
+{
+	while (1)
+	{
+		sleep(2);
+		printf("Your turn.\n");
+	}
+	return (NULL);
+}
+
+int main(void)
+{
+	pthread_t	newthread;
+
+	pthread_create(&newthread, NULL, my_turn, NULL);
+	//my_turn();
+	your_turn();
+	//sleep(12);
+	//printf("Fin\n");
+	return (0);
+}
