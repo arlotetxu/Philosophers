5514d5562ce3677dd96398e81477ff3f64db47e8
arlotetxu <96924120+arlotetxu@users.noreply.github.com>
2023-07-13 16:21:05 +0000

    Merging

--- learning/first_thread.c
+++ learning/first_thread.c
@@ -38,8 +38,6 @@ int main(void)
 	//my_turn();
 	your_turn();
 	pthread_join(newthread, NULL); //Espera a que el hilo de ejecucion termine para continuar con el codigo de main
-	//sleep(5);
-	//your_turn();
 	sleep(12);
 	//printf("Fin\n");
 	return (0);
